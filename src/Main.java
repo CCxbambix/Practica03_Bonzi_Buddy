2dc05753806be380eb6869f6b4d42522729e5681
NatzuF <GabrielCA_19@ciencias.unam.mx>
2025-03-14 12:59:02 -0600

    Agrego las clases que debemos complementar

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Clase principal que inicia la heladería Rotsy.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MenuHelados menu = new MenuHelados();
+        menu.mostrarMenu();
+    }
+}
